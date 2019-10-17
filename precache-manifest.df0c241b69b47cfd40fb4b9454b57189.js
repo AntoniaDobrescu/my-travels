c858e0fcf6594650a05d9511563aabea35494027
AntoniaDobrescu <antonia.dobrescu91@gmail.com>
2019-10-17 14:57:29 +0300

    Updates

---
+++ precache-manifest.df0c241b69b47cfd40fb4b9454b57189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41123e9fd46cee35e6d6204b4069e3c",
+    "url": "/my-travels/index.html"
+  },
+  {
+    "revision": "bd464ad1b04457e6eed3",
+    "url": "/my-travels/static/css/main.8a5c86e2.chunk.css"
+  },
+  {
+    "revision": "1adfd889fd655216a681",
+    "url": "/my-travels/static/js/2.b600eb30.chunk.js"
+  },
+  {
+    "revision": "bd464ad1b04457e6eed3",
+    "url": "/my-travels/static/js/main.da3fd932.chunk.js"
+  },
+  {
+    "revision": "a7494290411757e23a1e",
+    "url": "/my-travels/static/js/runtime-main.79b16804.js"
+  }
+]);
