1757f263ea6f80f19bfee47d67773642637f0a92
AntoniaDobrescu <antonia.dobrescu91@gmail.com>
2019-10-22 11:43:29 +0300

    used the map method

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,12 +1,51 @@
 import React from 'react';
 
-const Travel = (props) => {
+const travel = [
+  {
+    destination: 'Sibiu',
+    country: 'Romania',
+    img: require('../img/sibiu.jpg'),
+    distance: '1500 km'
+  },
+  {
+    destination: 'Pompei',
+    country: 'Italy',
+    img: require('../img/pompei.jpg'),
+    distance: '2436 km'
+  },
+  {
+    destination: 'Amsterdam',
+    country: 'Holland',
+    img: require('../img/amsterdam.jpg'),
+    distance: '1756 km'
+  },
+  {
+    destination: 'Berlin',
+    country: 'Germany',
+    img: require('../img/berlin.jpg'),
+    distance: '1120 km'
+  },
+  {
+    destination: 'Porto',
+    country: 'Portugal',
+    img: require('../img/porto.jpg'),
+    distance: '1500 km'
+  },
+];
 
-  return <div>
-     <p>Your destination: {props.destination}</p>
-      <p>Contry: {props.country}</p>
-      <img className='img' src={props.photo}/>
-      <p>Destination: {props.distance}</p>
-  </div>
+const Travel = () => {
+
+  return (<div>
+    {
+      travel.map((travel, i) => {
+        return <React.Fragment key={i}>
+          <p>{travel.destination}</p>
+          <p>{travel.country}</p>
+          <img src={travel.img}/>
+          <p>{travel.distance}</p>
+        </React.Fragment>
+      })
+    }
+  </div>)
 };
 export default Travel;
